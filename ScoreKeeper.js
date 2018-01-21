8260e54d2a10ba57bcae265575b87cfe135f2bad
Neoh213 <djrumph@gmail.com>
2018-01-20 19:49:55 -0500

    player two working as well

--- ScoreKeeper.js
+++ ScoreKeeper.js
@@ -1,11 +1,19 @@
 var p1Button = document.querySelector("#p1");
 var p2Button = document.getElementById("p2");
 var h1 = document.querySelector("h1");
-var p1BDispay = document.getElementById("p1Display");
+var p1BDisplay = document.getElementById("p1Display");
+var p2BDisplay = document.getElementById("p2Display");
 var p1Score = 0;
+var p2Score = 0;
 
 p1Button.addEventListener("click", function(){
   p1Score++;
   p1Display.textContent = p1Score;
+
+})
+
+p2Button.addEventListener("click", function(){
+  p2Score++;
+  p2Display.textContent = p2Score;
 
 })
