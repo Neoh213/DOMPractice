bb0bb708efbe42ac249638cbed4d2847d3317da0
Neoh213 <djrumph@gmail.com>
2018-01-20 20:00:10 -0500

    almost there

--- ScoreKeeper.js
+++ ScoreKeeper.js
@@ -5,15 +5,32 @@ var p1BDisplay = document.getElementById("p1Display");
 var p2BDisplay = document.getElementById("p2Display");
 var p1Score = 0;
 var p2Score = 0;
+var gameover = false;
+var winningScore = 5;
 
 p1Button.addEventListener("click", function(){
-  p1Score++;
-  p1Display.textContent = p1Score;
+  if(!gameover){
 
+
+  p1Score++;
+  if(p1Score === winningScore)
+  {
+    gameover = true;
+  }
+  p1Display.textContent = p1Score;
+}
 })
 
 p2Button.addEventListener("click", function(){
+  if(!gameover){
+
+
   p2Score++;
+  if(p2Score === winningScore)
+  {
+    gameover = true;
+  }
   p2Display.textContent = p2Score;
+}
 
 })
