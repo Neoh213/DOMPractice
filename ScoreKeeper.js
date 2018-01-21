36d610147b6b85722d861164d01bc558ebb1e123
Neoh213 <djrumph@gmail.com>
2018-01-21 01:50:00 -0500

    updates

--- ScoreKeeper.js
+++ ScoreKeeper.js
@@ -1,5 +1,6 @@
 var p1Button = document.querySelector("#p1");
 var p2Button = document.getElementById("p2");
+var resetButton = document.getElementById("reset");
 var h1 = document.querySelector("h1");
 var p1BDisplay = document.getElementById("p1Display");
 var p2BDisplay = document.getElementById("p2Display");
@@ -16,10 +17,11 @@ p1Button.addEventListener("click", function(){
   if(p1Score === winningScore)
   {
     gameover = true;
+    p1Display.classList.add("winner");
   }
   p1Display.textContent = p1Score;
 }
-})
+});
 
 p2Button.addEventListener("click", function(){
   if(!gameover){
@@ -29,8 +31,22 @@ p2Button.addEventListener("click", function(){
   if(p2Score === winningScore)
   {
     gameover = true;
+    p2Display.classList.add("winner");
   }
   p2Display.textContent = p2Score;
 }
 
-})
+});
+
+resetButton.addEventListener("click", function(){
+  p1Score = 0;
+  p2Score =0;
+  p1Display.textContent = p2Score;
+  p2Display.textContent = p2Score;
+  p1Display.classList.remove("winner");
+  p2Display.classList.remove("winner");
+  gameover = false;
+
+
+
+});
